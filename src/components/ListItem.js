6b009e7e21a789504c8a5fa59c3989fde350ddf8
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2019-06-16 12:06:10 +0200

    Log clicked element using action, reducer and touchableWithoutFeedback

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
 import { CardItem } from './common';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class ListItem extends Component {
-    
-    render() {
-        const { id, title } = this.props.library.item;
-        return (
-            <CardItem>
-                <Text> {title} {id}</Text>
-            </CardItem>
-        );
-    }
+  render() {
+    const { id, title } = this.props.library.item;
+    const { titleStyle } = styles;
+
+    return (
+      <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+        <View>
+          <CardItem>
+            <Text style={titleStyle}>
+              {id} - {title}
+            </Text>
+          </CardItem>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
 }
 
-export default ListItem;
+const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 20,
+    paddingLeft: 15
+  }
+});
+
+export default connect(
+  null,
+  actions
+)(ListItem);
